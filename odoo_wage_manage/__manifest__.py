0b08a918cd65ef444800c3d91907f7e633fcf1a1
Feng <suxuefeng20@gmail.com>
2019-09-04 13:48:55 +0800

    ...

--- odoo_wage_manage/__manifest__.py
+++ odoo_wage_manage/__manifest__.py
@@ -22,7 +22,7 @@
     'category': 'wage',
     'version': '1.0',
     'depends': ['base', 'web_progress', 'hr', 'mail'],
-    'installable': True,
+    'installable': False,
     'application': False,
     'auto_install': False,
     'data': [
